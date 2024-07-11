6c8e379583eba06b865fbb4567324c6761a5ecec
Logonz <logon.q@gmail.com>
2024-07-12 00:14:47 +0200

    Added a comment

--- init.lua
+++ init.lua
@@ -309,6 +309,7 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      -- Defaults
       --      signs = {
       --        add = { text = '+' },
       --        change = { text = '~' },
