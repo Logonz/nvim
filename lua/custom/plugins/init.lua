af42a5180104c212d53b33dc8536d49574174fc1
Logonz <logon.q@gmail.com>
2024-07-11 22:11:16 +0200

    Added undo, quit button, also created more ways to trigger undo event chunks

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Insert keybinds
+-- This adds undo to insert
+vim.keymap.set('i', '<C-z>', function() vim.cmd("undo") end)
+
+-- Normal keybinds
+vim.keymap.set('n', '<C-q>', function() vim.cmd("q") end)
+
+-- Undo specific rebinds, it makes it so that multiple undo points are created for the characters below
+--
+local opts = { noremap = true, silent = true }
+vim.keymap.set('i', ',', ',<C-G>u', opts)
+vim.keymap.set('i', '.', '.<C-G>u', opts)
+vim.keymap.set('i', ';', ';<C-G>u', opts)
+vim.keymap.set('i', ':', ':<C-G>u', opts)
+vim.keymap.set('i', ']', ']<C-G>u', opts)
+vim.keymap.set('i', ')', ')<C-G>u', opts)
+vim.keymap.set('i', '[', '[<C-G>u', opts)
+vim.keymap.set('i', ']', ']<C-G>u', opts)
+vim.keymap.set('i', '{', '{<C-G>u', opts)
+vim.keymap.set('i', '}', '}<C-G>u', opts)
+vim.keymap.set('i', '=', '=<C-G>u', opts)
+vim.keymap.set('i', "'", "'<C-G>u", opts) -- '
+vim.keymap.set('i', '"', '"<C-G>u', opts) -- "
+vim.keymap.set('i', '<Space>', '<Space><C-G>u', opts)
+-- vim.keymap.set('i', '<Tab>', '<Tab><C-G>u', opts)
+vim.keymap.set('i', '<CR>', '<CR><C-G>u', opts)
+
 return {}
