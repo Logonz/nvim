2aaaec7e12887cb0270b8d3e8cdc55ecb4d566c8
Logonz <logon.q@gmail.com>
2024-07-14 15:41:39 +0200

    Added a more visible way to see my binds vs standard binds

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,21 +1,48 @@
 -- Here we have general keybinds
+-- Default: `▎` ~
+-- Alternatives: ~
+-- • left aligned solid
+--  • `▏`
+--  • `▎` (default)
+--  • `▍`
+--  • `▌`
+--  • `▋`
+--  • `▊`
+--  • `▉`
+--  • `█`
+-- • center aligned solid
+--  • `│`
+--  • `┃`
+-- • right aligned solid
+--  • `▕`
+--  • `▐`
+-- • center aligned dashed
+--  • `╎`
+--  • `╏`
+--  • `┆`
+--  • `┇`
+--  • `┊`
+--  • `┋`
+-- • center aligned double
+--  • `║`
+--
 -- Insert keybinds
 -- This adds undo to insert
 -- vim.keymap.set('i', '<C-z>', function() vim.cmd("undo") end)
 vim.keymap.set('i', '<C-z>', "<CMD>undo<CR>")
-vim.keymap.set('i', '<C-s>', "<Esc><CMD>w<CR>", { desc = "Exit insert mode and [S]ave" })
+vim.keymap.set('i', '<C-s>', "<Esc><CMD>w<CR>", { desc = "▌ Exit insert mode and [S]ave" })
 vim.keymap.set('i', '<Home>', "<C-o>^") -- <C-o> enters "one-command" mode allowing one normal command before going back to insert
 
 -- Normal keybinds
-vim.keymap.set('n', '<C-q>', "<CMD>q<CR>", { desc = "Quit current buffer" })
-vim.keymap.set('n', '<C-s>', "<CMD>w<CR>", { desc = "[S]ave" })
+vim.keymap.set('n', '<C-q>', "<CMD>q<CR>", { desc = "▌ Quit current buffer" })
+vim.keymap.set('n', '<C-s>', "<CMD>w<CR>", { desc = "▌ [S]ave" })
 vim.keymap.set('n', '<Home>', "^")
 vim.keymap.set('n', "<leader>'", "<Plug>(comment_toggle_linewise_current)",
-  { remap = true, desc = "Comment current line" })
+  { remap = true, desc = "▌ Comment current line" })
 
 -- Visual keybinds
 vim.keymap.set('x', "<leader>'", "<Plug>(comment_toggle_linewise_visual)",
-  { remap = true, desc = "Comment selected lines" })
+  { remap = true, desc = "▌ Comment selected lines" })
 
 -- Undo specific rebinds, it makes it so that multiple undo points are created for the characters below
 --
